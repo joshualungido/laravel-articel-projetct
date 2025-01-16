ec5d85f35e14d70b8ccba54cab7e6777517fdbbe
joshualungido <joshualungido@gmail.com>
2025-01-16 15:32:39 +0700

    Initial commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    protected $model = \App\Models\Post::class; // Tambahkan ini jika belum ada
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(6),
+            'author_id' => User::factory(),
+            'category_id' => Category::factory(),
+            'slug' => Str::slug(fake()->sentence()),
+            'body' => fake()->text(),
+        ];
+    }
+}
